991ad06e7b7e11114e378f4adea97b3077d765f4
smalta99 <75386345+smalta99@users.noreply.github.com>
2024-06-18 03:42:17 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -43,7 +43,7 @@ function displayConfig(config) {
     config.layers.forEach((layer, index) => {
         const dataRate = calculateDataRate(layer.segments, layer.modulation, config.guardInterval, layer.codeRate);
         resultDiv.innerHTML += `
-            <h3>Capa ${layer}</h3>
+            <h3>Capa ${layers}</h3>
             <p>Tasa de Datos: ${dataRate.toFixed(2)} Mbps</p>
         `;
     });
